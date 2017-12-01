5dc17a3e4aa45c56a5bf0b197ee0e156b366b73c
yuda <yudarahman.jtk12@gmail.com>
2017-12-01 18:53:58 +0700

    Initial Commit

---
+++ Belajar Single View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Belajar Single View
+//
+//  Created by Yuda on 12/1/17.
+//  Copyright © 2017 Yuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
